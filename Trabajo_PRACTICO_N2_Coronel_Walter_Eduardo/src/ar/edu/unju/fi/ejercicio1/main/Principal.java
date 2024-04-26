22ec253fe9f1f0f4ca8108a5ada77880f8d5048e
waltopo <waltopo@DESKTOP-NVOFF02>
2024-04-26 20:37:17 -0300

    intento de raiz

---
+++ Trabajo_PRACTICO_N2_Coronel_Walter_Eduardo/src/ar/edu/unju/fi/ejercicio1/main/Principal.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio1.main;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Trabajo_PRACTICO_N2_Coronel_Walter_Eduardo/src/ar/edu/unju/fi/ejercicio1/model/package-info.java
@@ -0,0 +1,1 @@
+package ar.edu.unju.fi.ejercicio1.model;
